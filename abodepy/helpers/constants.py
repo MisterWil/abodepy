3b8b0228fcf2a48f2b2ade704ae31d225d7dece1
Milad Soufastai <milad-soufastai@users.noreply.github.com>
2019-10-08 07:51:48 -0700

    Fixing the Camera's Snapshot API (#47)
    * Fixing Capture for new Abode API
    
    * Fixing Capture for new Abode API

--- abodepy/helpers/constants.py
+++ abodepy/helpers/constants.py
@@ -76,7 +76,8 @@ AUTOMATION_ID_URL = AUTOMATION_URL + '$AUTOMATIONID$/'
 AUTOMATION_EDIT_URL = AUTOMATION_ID_URL + 'edit'
 AUTOMATION_APPLY_URL = AUTOMATION_ID_URL + 'apply'
 
-CAMS_ID_CAPTURE_URL = BASE_URL + 'api/v1/cams/$DEVID$/capture'
+CAMS_ID_CAPTURE_URL = BASE_URL + 'api/v1/cams/$DEVID$/snapshot'
+
 
 TIMELINE_IMAGES_ID_URL = BASE_URL + \
     'api/v1/timeline?device_id=$DEVID$&dir=next' + \
